bd6bc17367781a9b37935e8483086d06beddffee
Gianmarco Luppi <gianmarco.luppi@sky.uk>
2022-07-19 10:12:19 +0100

    TMP-2 initial springBoot commit

---
+++ src/main/java/com/sky/tempest/TempestApplication.java
@@ -0,0 +1,13 @@
+package com.sky.tempest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempestApplication.class, args);
+	}
+
+}
